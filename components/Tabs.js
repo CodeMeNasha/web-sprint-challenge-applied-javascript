4854e6a748a872d311d81563ea0187359c816767
QDG <nasha@QDGs-MacBook-Air.local>
2020-09-04 09:02:29 -0700

    tabs done

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,27 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+const require = document.querySelector(".topics");
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then( response => {
+        console.log(response);
+        response.data.topics.forEach( topic => {
+            require.appendChild(oneTab(topic))
+        })
+    })
+    .catch( error => {
+        console.log(error);
+    })
+
+function oneTab(tabTitle) {
+    const tab = document.createElement("div");
+
+    tab.classList.add("tab");
+
+    tab.textContent = tabTitle;
+
+    return tab;
+}
